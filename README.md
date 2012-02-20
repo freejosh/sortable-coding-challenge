d9870eaa5ca93207f41686ea17df7a9ecf4e14c2
Josh Freeman <freejosh@gmail.com>
2012-02-20 11:26:30 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sortable Coding Challenge
+
+My entry for the Sortable coding challenge at http://sortable.com/blog/coding-challenge/.
+
+## Usage
+
+Clone and run `index.php`, either through the command line or by visiting it in a browser.
+Input: `listings.txt` and `products.txt` should be in the same directory as the script.
+Output: writes `results.txt` in the same directory as the script.
---
+++ index.php
@@ -0,0 +1,98 @@
+<?php
+set_time_limit(0);
+
+// replaces non-word characters with optional non-word match, and inserts optional non-word match between numbers and letters
+function prepareRegex($str) {
+	return preg_replace('/([a-z])([0-9])/i', '$1\W?$2',
+				 preg_replace('/([0-9])([a-z])/i', '$1\W?$2',
+				 preg_replace('/\W/', '\W?',
+				 $str)));
+}
+
+// make regex string with word boundaries
+function makeRegex($str) {
+	return '/\b'.prepareRegex($str).'\b/i';
+}
+
+// make regex string without word boundaries
+function makeRegexNoWord($str) {
+	return '/'.prepareRegex($str).'/i';
+}
+
+// initialize output
+$output = array();
+define('OUTPUT_FILE', 'results.txt');
+file_put_contents(OUTPUT_FILE, '');
+
+// read in listings
+$listings = file_get_contents('listings.txt');
+$listings = explode("\n", $listings);
+foreach($listings as &$l) $l = json_decode($l, true);// decode json string for each lisitng
+
+// read in products
+$products = file_get_contents('products.txt');
+$products = explode("\n", $products);
+foreach($products as $j => &$p) {
+	$p = json_decode($p, true);// decode json string
+	if (is_null($p)) {
+		unset($products[$j]);// remove product if json decode failed
+		continue;
+	}
+
+	// initialize product in output
+	$output[$p['product_name']] = array(
+		'product_name' => $p['product_name'],
+		'listings' => array()
+	);
+
+	// search for ideal match first - manufacturer + family + full model
+	if (empty($p['manufacturer']) || empty($p['model']) || empty($p['family'])) continue;
+
+	$manufacturer = makeRegex($p['manufacturer']);
+	$model = makeRegex($p['model']);
+	$family = makeRegex($p['family']);
+
+	foreach($listings as $i => $l) {
+		if (!preg_match($manufacturer, $l['manufacturer']) || !preg_match($model, $l['title']) || !preg_match($family, $l['title'])) continue;
+
+		$output[$p['product_name']]['listings'][] = $l;// add match to output
+		unset($listings[$i]);// remove matched listing from future searches
+	}
+}
+
+// 2nd pass - look for manufacturer + full model from listings that haven't been matched yet
+foreach($products as $p) {
+	if (empty($p['manufacturer']) || empty($p['model'])) continue;
+
+	$manufacturer = makeRegex($p['manufacturer']);
+	$model = makeRegex($p['model']);
+
+	foreach($listings as $i => $l) {
+		if (!preg_match($manufacturer, $l['manufacturer']) || !preg_match($model, $l['title'])) continue;
+
+		$output[$p['product_name']]['listings'][] = $l;
+		unset($listings[$i]);
+	}
+}
+
+// 3rd pass - look for partial models - e.g. model is DSC123 but listing has DSC123S
+// skipped if model is only numbers or only letters (too general - many false positives)
+foreach($products as $p) {
+	if (empty($p['manufacturer']) || empty($p['model']) || is_numeric($p['model']) || !preg_match('/\d/', $p['model'])) continue;
+
+	$manufacturer = makeRegex($p['manufacturer']);
+	$model = makeRegexNoWord($p['model']);
+
+	foreach($listings as $i => $l) {
+		if (!preg_match($manufacturer, $l['manufacturer']) || !preg_match($model, $l['title'])) continue;
+
+		$output[$p['product_name']]['listings'][] = $l;
+		unset($listings[$i]);
+	}
+}
+
+// write output
+foreach($output as $line) file_put_contents(OUTPUT_FILE, json_encode($line)."\n", FILE_APPEND);
+
+exit(0);
+?>
